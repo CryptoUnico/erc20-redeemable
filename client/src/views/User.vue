6223dded05de3f2bb80effa0740fcbc87692cebc
uni <CryptoUnico@gmail.com>
2021-02-06 23:03:46 -0600

    ROBOT

--- client/src/views/User.vue
+++ client/src/views/User.vue
@@ -24,7 +24,7 @@
               <Icon name="external-link" class="ml-1" />
             </p>
           </a>
-          <Block :slim="true" title="Pending BAL">
+          <Block :slim="true" title="Pending ROBOT">
             <div class="overflow-hidden">
               <div
                 v-for="(dist, week, i) in unclaimed"
@@ -35,7 +35,7 @@
                 <div class="flex-auto">
                   <a
                     :href="
-                      `https://github.com/balancer-labs/bal-mining-scripts/blob/master/reports/${_week(
+                      `https://github.com/cryptounico/bal-mining-scripts/blob/master/reports/${_week(
                         week
                       )}/_totals.json`
                     "
@@ -45,20 +45,20 @@
                     <Icon name="external-link" class="ml-1" />
                   </a>
                 </div>
-                <div>{{ $n(dist) }} BAL</div>
+                <div>{{ $n(dist) }} ROBOT</div>
               </div>
               <p
                 v-if="Object.keys(unclaimed).length === 0"
                 class="p-4 m-0 d-block"
               >
-                There isn't any pending BAL here.
+                There isn't any pending ROBOT here.
               </p>
             </div>
           </Block>
           <Block
             v-if="Object.keys(claimed).length > 0"
             :slim="true"
-            title="Claimed BAL"
+            title="Claimed ROBOT"
           >
             <div class="overflow-hidden">
               <div
@@ -80,16 +80,16 @@
                     <Icon name="external-link" class="ml-1" />
                   </a>
                 </div>
-                <div>{{ $n(dist) }} BAL</div>
+                <div>{{ $n(dist) }} ROBOT</div>
               </div>
             </div>
           </Block>
         </div>
         <div class="col-12 col-lg-4 float-left">
-          <Block title="Total pending BAL">
+          <Block title="Total pending ROBOT">
             <div class="mb-2">
               <UiButton class="width-full mb-2">
-                {{ $n(totalUnclaimed) }} BAL
+                {{ $n(totalUnclaimed) }} ROBOT
               </UiButton>
             </div>
             <UiButton
